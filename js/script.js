fc843dfb0fbaf0de9d71cf9abf5b2b2eb3f6b97d
Julia <126119314+Jubiluka@users.noreply.github.com>
2026-03-23 23:56:19 -0700

    Update script.js null check bug fix

--- js/script.js
+++ js/script.js
@@ -1,8 +1,11 @@
-document.getElementById("colorButton").addEventListener("click", () => {
+const colorButton = ocument.getElementById("colorButton");
+if (colorButton) {
+  colorButton.addEventListener("click", () => {
   const colors = ["#fce4ec", "#f3e5f5", "#e3f2fd", "#e8f5e9", "#fff3e0"];
   const random = colors[Math.floor(Math.random() * colors.length)];
   document.body.style.backgroundColor = random;
 });
+}
 
 //PHOTO GALLERY CODE vvv
 document.addEventListener("DOMContentLoaded", () => {
